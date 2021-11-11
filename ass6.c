dfd9ebeb4ea12034bc551a8da2cde0c1b79af028
Rushikesh <rushikeshjyoti14@gmail.com>
2021-11-11 16:48:29 +0530

    updated input file for lexer

--- ass6.c
+++ ass6.c
@@ -1,9 +1,17 @@
 void main(){
-    int age = 20;
-    if(age >= 18){
-        printf("You are an adult.");
+    char name[] = "Rushikesh";
+    int age = 20, IQ = 200;
+
+    if(age >= 18 && IQ > 100){
+        printf("You are a smart adult.");
+    }
+    else if(age >= 18 && IQ < 100){
+        printf("You are an adult");
     }
     else {
         printf("Too young!");
+
+        if(IQ > 100)
+            printf("But smart");
     }
 }
