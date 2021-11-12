92eb1677897683a3aa6d182629e92aca9628e7f9
Rushikesh <rushikeshjyoti14@gmail.com>
2021-11-12 11:58:04 +0530

    added comments to input file for lexer

--- ass6.c
+++ ass6.c
@@ -2,7 +2,12 @@ void main(){
     char name[] = "Rushikesh";
     int age = 20, IQ = 200;
 
+    /*
+        This is a multiline comment
+    */
+
     if(age >= 18 && IQ > 100){
+        // I have the big brain
         printf("You are a smart adult.");
     }
     else if(age >= 18 && IQ < 100){
